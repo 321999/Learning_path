481f39251af968e35b022b64a3213fe0a7325e3a
viraj <yuvrajchindarkar90@gmail.com>
2023-07-21 23:21:34 +0530

    update fullstack roadmap

--- src/Description/PopupPage.js
+++ src/Description/PopupPage.js
@@ -1,11 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { AiOutlineClose } from 'react-icons/ai';
 import { IoMdArrowDropdown } from 'react-icons/io';
 import '../Style/style.css';
 
-const PopupPage = () => {
+const PopupPage = ({ selectedTopic, onClose, open }) => {
   const [selectedOption, setSelectedOption] = useState(null);
   const [showOptions, setShowOptions] = useState(false);
+  const [isTopicVisible, setIsTopicVisible] = useState(true);
+
+  useEffect(() => {
+    setIsTopicVisible(open); // Update the isTopicVisible state based on the open prop
+    setSelectedOption(null); // Reset the selectedOption when the popup is closed
+    setShowOptions(false); // Hide the options dropdown when the popup is closed
+  }, [open]);
 
   const toggleOptions = () => {
     setShowOptions(!showOptions);
@@ -16,11 +23,13 @@ const PopupPage = () => {
     setShowOptions(false);
   };
 
-  return (
-    <div className='mycontent'> 
-
-    <div> 
-    <div className="popup-container p-20 mx-auto px-6 sm:px-70 py-8">
+  const handleClose = () => {
+    setIsTopicVisible(false);
+    onClose(); // Call the onClose function passed from parent to inform it that the topic is closed
+  };
+  return isTopicVisible && selectedTopic ? (
+    <div className="mycontent popup-container p-20 mx-auto px-6 sm:px-70 py-8">
+      <div>
       <div className="mb-2">
         <div className="relative inline-flex rounded-md border border-gray-300">
           <span className="inline-flex cursor-default items-center p-1 px-2 text-sm text-black">
@@ -29,6 +38,7 @@ const PopupPage = () => {
             </span>
             <span className="ml-2 capitalize">{selectedOption || 'pending'}</span>
           </span>
+
           <button
             className="inline-flex cursor-pointer items-center rounded-br-md rounded-tr-md border-l border-l-gray-300 bg-gray-100 p-1 px-2 text-sm text-black hover:bg-gray-200"
             onClick={toggleOptions}
@@ -37,12 +47,17 @@ const PopupPage = () => {
             <IoMdArrowDropdown className="h-4 w-4" />
           </button>
         </div>
-        <button type="button" id="close-topic" className="absolute right--2px top-20px inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900">
+        <button
+          type="button"
+          id="close-topic"
+          className="absolute right-2 top-2 inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900"
+          onClick={handleClose}
+        >
           <AiOutlineClose className="h-5 w-5" />
         </button>
       </div>
       {showOptions && (
-        <div className="absolute right--20px top-20px mt-1 flex min-w-[160px] flex-col divide-y rounded-md border border-gray-200 bg-white shadow-md">
+        <div className="absolute right-20 top-20 mt-1 flex min-w-[160px] flex-col divide-y rounded-md border border-gray-200 bg-white shadow-md">
           <button
             className={`inline-flex justify-between px-3 py-1.5 text-left text-sm text-gray-800 hover:bg-gray-100 ${
               selectedOption === 'Done' ? 'bg-green-100' : ''
@@ -78,46 +93,25 @@ const PopupPage = () => {
           </button>
         </div>
       )}
-    </div>
 
-    <div className="cont">  
-        <main id="main-content">
-            <h1 className="text-2xl mb-0.5 font-bold sm:mb-2 sm:text-4xl">Internet</h1>
-            <p className="text-sm text-black-500 sm:text-lg mb-4">
-                The Internet is a global network of computers connected to each other which communicate through a standardized set of protocols.
-            </p>
-            <p className="text-sm text-black-500 sm:text-lg mb-4">Visit the following resources to learn more:</p>
-            <ul className="list-disc list-inside">
-                <li>
-                    <a href="https://cs.fyi/guide/how-does-internet-work" target="_blank" rel="noopener noreferrer">
-                        How does the Internet Work?
-                    </a>
-                </li>
-                <li>
-                    <a href="https://developer.mozilla.org/en-US/docs/Learn/Common_questions/How_does_the_Internet_work" target="_blank" rel="noopener noreferrer">
-                        How Does the Internet Work? MDN Docs
-                    </a>
-                </li>
-                <li>
-                    <a href="/guides/what-is-internet">Introduction to Internet</a>
-                </li>
-                <li>
-                    <a href="https://www.youtube.com/watch?v=TNQsmPf24go" target="_blank" rel="noopener noreferrer">
-                        How does the Internet work?
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.youtube.com/watch?v=7_LPdttKXPc" target="_blank" rel="noopener noreferrer">
-                        How the Internet Works in 5 Minutes
-                    </a>
-                </li>
-            </ul>
-        </main>
+        <h1 className="text-2xl mb-0.5 font-bold sm:mb-2 sm:text-4xl" id="infoTitle">{selectedTopic.title}</h1>
+        <p className="text-sm text-black-500 sm:text-lg mb-4" id="infoDescription">{selectedTopic.description}</p>
+        <p className="text-sm text-black-500 sm:text-lg mb-4">{selectedTopic.subtitle}</p>
+      
+        {selectedTopic.links && (
+          <ul className="list-disc list-inside">
+            {selectedTopic.links.map((link, index) => (
+              <li key={index}>
+                <a href={link} target="_blank" rel="noopener noreferrer">
+                  Link {index + 1}
+                </a>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
-
-    </div>
-</div>
-  );
+  ) : null;
 };
 
 export default PopupPage;
--- src/roadmap/Fullstack.js
+++ src/roadmap/Fullstack.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import '../Style/Fullstack.css'
+import React, { useState } from 'react';
+import '../Style/Fullstack.css';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import PopupPage from '../Description/PopupPage';
+import topicsInfo from '../data/topicsInfo.json';
+
 
 
 const Fullstack = () => {
-    
+    const [selectedTopic, setSelectedTopic] = useState(null);
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+  
+    const showInfo = (topicName) => {
+      setSelectedTopic(topicsInfo[topicName]);
+      setIsPopupOpen(true); // Set isPopupOpen to true to open the popup
+    };
+  
+    const handleCloseTopic = () => {
+      setIsPopupOpen(false); // Set isPopupOpen to false to close the popup
+    };
   return (
 <div>
          <div className="timeline1 mx-auto px-60 sm:px-70 bg-blue-100 py-8">  
@@ -18,52 +31,61 @@ const Fullstack = () => {
           <div className="card">
           <div className="card--0">
             <div className="info">
-            <h3 className="titl font-bold"><a href='#'>Frontend</a><span style={{color:'green', padding:'10px'}}><CheckCircleIcon/></span></h3>
+           <button className="titl font-bold" onClick={() => showInfo('frontend')} >Frontend<span style={{ color: 'green', padding: '10px' }}><CheckCircleIcon /> </span> </button>        
             </div>
             </div>
 
           <div className="card0">
             <div className="info">
-            <h3 className="font-bold"><a href='#'>HTML</a><span style={{color:'green', padding:'10px'}}><CheckCircleIcon/></span></h3>
+            <button
+                  className="titl font-bold"
+                  onClick={() => showInfo('html')}
+                >
+                  HTML
+                  <span style={{ color: 'green', padding: '10px' }}>
+                    <CheckCircleIcon />
+                  </span>
+                </button>   
             </div>
             </div>
             <div className="card-0">
             <div className="info">
-            <h3 className="font-bold"><a href='#'>CSS</a><span style={{color:'green', padding:'10px'}}><CheckCircleIcon/></span></h3>
+            <button className="titl font-bold" onClick={() => showInfo('css')} >CSS<span style={{ color: 'green', padding: '10px' }}><CheckCircleIcon /> </span> </button>        
             </div>
             </div>
           </div>
 
           <div className="card">
           <div  className='card-3'>
-            <div className="info"> 
-              <h3 className="title-1 font-bold"><a href='#'>Project1</a><span style={{color:'green', padding:'10px'}}><CheckCircleIcon/></span></h3> 
-            </div>
+            <div className="info">
+            <button className="title-1 font-bold" onClick={() => showInfo('project1')} >Project1<span style={{ color: 'green', padding: '10px' }}><CheckCircleIcon /> </span></button>        
+             </div>
             </div>
             <div class="line"></div>
 
             <div className="card-1">
             <div className="info">
-            <h3 className="font-bold"><a href='#'>JAVASCRIPT</a><span style={{color:'green', padding:'10px'}}><CheckCircleIcon/></span></h3>
+              
+            <button className="titl font-bold" onClick={() => showInfo('js')} >JavaScript<span style={{ color: 'green', padding: '10px' }}><CheckCircleIcon /> </span> </button>        
             </div>
             </div>
 
             <div  className='card-4'>
             <div className="info"> 
-              <h3 className="title-2 font-bold"><a href='#'>Project2</a><span style={{color:'green', padding:'10px'}}><CheckCircleIcon/></span></h3> 
+            <button className="title-2 font-bold" onClick={() => showInfo('project2')} >Project2<span style={{ color: 'green', padding: '10px' }}><CheckCircleIcon /> </span></button>        
             </div>
             </div>
             <div class="line-1"></div>
 
             <div className="card-5">
             <div className="info">
-            <h3 className="font-bold"><a href='#'>NPM</a><span style={{color:'green', padding:'10px'}}><CheckCircleIcon/></span></h3>
+            <button className="titl font-bold" onClick={() => showInfo('npm')} >NPM<span style={{ color: 'green', padding: '10px' }}><CheckCircleIcon /> </span> </button>        
             </div>
             </div>
 
             <div  className='card-6'>
             <div className="info"> 
-              <h3 className="title-3 font-bold"><a href='#'>Project3</a><span style={{color:'green', padding:'10px'}}><CheckCircleIcon/></span></h3> 
+            <button className="title-3 font-bold" onClick={() => showInfo('project3')} >Project3<span style={{ color: 'green', padding: '10px' }}><CheckCircleIcon /> </span></button>        
             </div>
             </div>
             <div class="line-2"></div>
@@ -73,26 +95,26 @@ const Fullstack = () => {
           <div className="card">
            <div className="card-7">
             <div className="info">
-            <h3 className="font-bold"><a href='#'>GIT/GITHUB</a><span style={{color:'green', padding:'10px'}}><CheckCircleIcon/></span></h3>
+            <button className="titl font-bold" onClick={() => showInfo('git/github')} >GIT/GITHUB<span style={{ color: 'green', padding: '10px' }}><CheckCircleIcon /> </span> </button>        
             </div>
             </div>
 
             <div className="card-8">
             <div className="info">
-            <h3 className="t font-bold"><a href='#'>BOOTSTRAP</a><span style={{color:'green', padding:'10px'}}><CheckCircleIcon/></span></h3>
+            <button className="titl font-bold" onClick={() => showInfo('bootstrap')} >Bootstrap<span style={{ color: 'green', padding: '10px' }}><CheckCircleIcon /> </span> </button>        
             </div>
             </div>
 
             <div  className='card-9'>
             <div className="info"> 
-              <h3 className="title-4 font-bold"><a href='#'>Project3</a><span style={{color:'green', padding:'10px'}}><CheckCircleIcon/></span></h3> 
+            <button className="title-4 font-bold" onClick={() => showInfo('project4')} >Project4<span style={{ color: 'green', padding: '10px' }}><CheckCircleIcon /> </span></button>        
             </div>
             </div>
             <div class="line-3"></div>
 
             <div className="card-10">
             <div className="info">
-            <h3 className="font-bold"><a href='#'>REACT</a><span style={{color:'green', padding:'10px'}}><CheckCircleIcon/></span></h3>
+            <button className="titl font-bold" onClick={() => showInfo('react')} >React<span style={{ color: 'green', padding: '10px' }}><CheckCircleIcon /> </span> </button>        
             </div>
             </div>
           </div>
@@ -100,26 +122,26 @@ const Fullstack = () => {
           <div className="card">
           <div  className='card-11'>
             <div className="info"> 
-              <h3 className="title-5 font-bold"><a href='#'>Project4 FRONTEND OVER</a><span style={{color:'green', padding:'10px'}}><CheckCircleIcon/></span></h3> 
+            <button className="title-5 font-bold" onClick={() => showInfo('project5')} >Project5<span style={{ color: 'green', padding: '10px' }}><CheckCircleIcon /> </span></button>        
             </div>
             </div>
             <div class="line-4"></div>
             
             <div className="card-12">
             <div className="info">
-            <h3 className="font-bold"><a href='#'>Backend</a><span style={{color:'green', padding:'10px'}}><CheckCircleIcon/></span></h3>
+            <button className="titl font-bold" onClick={() => showInfo('backend')} >Backend<span style={{ color: 'green', padding: '10px' }}><CheckCircleIcon /> </span> </button>        
             </div>
             </div>
 
             <div className="card-13">
             <div className="info">
-            <h3 className="t font-bold"><a href='#'>NODE.JS</a><span style={{color:'green', padding:'10px'}}><CheckCircleIcon/></span></h3>
+            <button className="titl font-bold" onClick={() => showInfo('node')} >Node.js<span style={{ color: 'green', padding: '10px' }}><CheckCircleIcon /> </span> </button>        
             </div>
             </div>
 
             <div  className='card-14'>
             <div className="info"> 
-              <h3 className="title-6 font-bold"><a href='#'>Project5</a><span style={{color:'green', padding:'10px'}}><CheckCircleIcon/></span></h3> 
+            <button className="title-6 font-bold" onClick={() => showInfo('project6')} >Project6<span style={{ color: 'green', padding: '10px' }}><CheckCircleIcon /> </span></button>        
             </div>
             </div>
             <div class="line-5"></div>
@@ -130,20 +152,20 @@ const Fullstack = () => {
 
           <div className="card-15">
             <div className="info">
-            <h3 className="font-bold"><a href='#'>EXPRESS.JS</a><span style={{color:'green', padding:'10px'}}><CheckCircleIcon/></span></h3>
+            <button className="titl font-bold" onClick={() => showInfo('express')} >EXPRESS.JS<span style={{ color: 'green', padding: '10px' }}><CheckCircleIcon /> </span> </button>        
             </div>
             </div>
 
             <div  className='card-16'>
             <div className="info"> 
-              <h3 className="title-7 font-bold"><a href='#'>Project6</a><span style={{color:'green', padding:'10px'}}><CheckCircleIcon/></span></h3> 
+            <button className="title-7 font-bold" onClick={() => showInfo('project7')} >Project7<span style={{ color: 'green', padding: '10px' }}><CheckCircleIcon /> </span></button>        
             </div>
             </div>
             <div class="line-6"></div>
 
             <div className="card-17">
             <div className="info">
-            <h3 className="font-bold"><a href='#'>DATABASE</a><span style={{color:'green', padding:'10px'}}><CheckCircleIcon/></span></h3>
+            <button className="titl font-bold" onClick={() => showInfo('databases')} >Database<span style={{ color: 'green', padding: '10px' }}><CheckCircleIcon /> </span> </button>        
             </div>
             </div>
           
@@ -152,32 +174,32 @@ const Fullstack = () => {
           <div className="card">
           <div className="card-18">
             <div className="info">
-            <h3 className="font-bold"><a href='#'>MONGOdB</a><span style={{color:'green', padding:'10px'}}><CheckCircleIcon/></span></h3>
+            <button className="titl font-bold" onClick={() => showInfo('MONGOdB')} >MONGOdB<span style={{ color: 'green', padding: '10px' }}><CheckCircleIcon /> </span> </button>        
             </div>
             </div>
 
             <div  className='card-19'>
             <div className="info"> 
-              <h3 className="title-8 font-bold"><a href='#'>Project6</a><span style={{color:'green', padding:'10px'}}><CheckCircleIcon/></span></h3> 
+            <button className="title-8 font-bold" onClick={() => showInfo('project8')} >Project8<span style={{ color: 'green', padding: '10px' }}><CheckCircleIcon /> </span></button>        
             </div>
             </div>
             <div class="line-7"></div>
 
             <div className="card-20">
             <div className="info">
-            <h3 className="font-bold"><a href='#'>RESTful APIs</a><span style={{color:'green', padding:'10px'}}><CheckCircleIcon/></span></h3>
+            <button className="titl font-bold" onClick={() => showInfo('rest')} >RESTFUL API<span style={{ color: 'green', padding: '10px' }}><CheckCircleIcon /> </span> </button>        
             </div>
             </div>
 
             <div className="card-21">
             <div className="info">
-            <h3 className="font-bold"><a href='#'>JWT Auth</a><span style={{color:'green', padding:'10px'}}><CheckCircleIcon/></span></h3>
+            <button className="titl font-bold" onClick={() => showInfo('jwt')} >JWT<span style={{ color: 'green', padding: '10px' }}><CheckCircleIcon /> </span> </button>        
             </div>
             </div>
 
             <div  className='card-22'>
             <div className="info"> 
-              <h3 className="title-9 font-bold"><a href='#'>Project6</a><span style={{color:'green', padding:'10px'}}><CheckCircleIcon/></span></h3> 
+            <button className="title-9 font-bold" onClick={() => showInfo('project9')} >Project9<span style={{ color: 'green', padding: '10px' }}><CheckCircleIcon /> </span></button>        
             </div>
             </div>
             <div class="line-8"></div>
@@ -185,6 +207,7 @@ const Fullstack = () => {
 
 </div>
 </div>
+<PopupPage selectedTopic={selectedTopic} onClose={handleCloseTopic} open={isPopupOpen} />
 </div>
 
   );
